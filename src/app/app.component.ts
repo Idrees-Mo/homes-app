aab2c8949bd2720da2ab175070578c321518a5fa
Muhammad Idrees <idris@y7mail.com>
2023-09-23 12:48:09 +1000

    used routes to render homes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   standalone: true,
   selector: "app-root",
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterOutlet],
   template: `<main>
     <header class="brand-name">
       <img
@@ -15,7 +16,8 @@ import { HomeComponent } from "./home/home.component";
       />
     </header>
     <section class="content">
-      <app-home></app-home>
+      <!-- <app-home></app-home> -->
+      <router-outlet></router-outlet>
     </section>
   </main>`,
   styleUrls: ["./app.component.css"],
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
-const routeConfig: Routes = [];
+const routeConfig: Routes = [{ path: "", component: HomeComponent }];
 
 export default routeConfig;
